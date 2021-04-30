c606d70f699031e39acf6e955b7a411370464f4f
Felipe Gonzalez Q <fgonzalez@lya-electronic.com>
2021-04-30 13:27:00 -0500

    a

--- openpyxl/excel_work.py
+++ openpyxl/excel_work.py
@@ -1,35 +1,40 @@
+import os 
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
-excel = Workbook() # creamos objeto de Excel
-excel.save('Doc_Excel.xlsx') 
+wb = Workbook() # creamos objeto de Excel
+wb.save('Doc_Excel.xlsx') 
 # se guarda un documento de excel en blanco con
-# una sola hoja llamada "sheet"
+# una sola ws llamada "sheet"
 
-# Para cargar una hoja de excel ya existente
-excel2 = load_workbook('Doc_Excel.xlsx')
-excel2.create_sheet('Primera_hoja',0)
+# Para cargar una ws de excel ya existente
+wb = load_workbook('Doc_Excel.xlsx')
+wb.create_sheet('Primera_ws',0)
 
 # Escribiremos valores en la primero columna
 for i in range(10):
-    excel2['Primera_hoja']['A{}'.format(i+1)] = i 
+    wb['Primera_ws']['A{}'.format(i+1)] = i 
 # Otra forma de cambiar los valores
-hoja = excel2['Primera_hoja']
-hoja.cell(row=6, column=1, value=30)
+ws = wb['Primera_ws']
+ws.cell(row=6, column=1, value='hola')
 
-excel2.save('Doc_Excel.xlsx')
+ws['B1'] = 1
+
+wb.save('Doc_Excel.xlsx')
 
 # Cambiarle el estilo a la pestaña de la ventana
-hoja = excel2['Primera_hoja']
-hoja.sheet_properties.tabColor = "1072AB"
+ws = wb['Primera_ws']
+ws.sheet_properties.tabColor = "1072AB"
 
 # Acceder al valor de una celda
-print(excel2['Primera_hoja']['A6'].value)
+print(wb['Primera_ws']['A6'].value)
 
 # se puede acceder a un rango de valores
-celdas = hoja['A2':'A7'] # guardamos el rengo de celdas
+celdas = ws['A2':'A7'] # guardamos el rengo de celdas
 # se guarda como una matrix
 print(celdas[5][0].value) # accedemos a cada valor individual 
 # de nuestra matrix de celdas
 
-excel2.save('Doc_Excel.xlsx')
+wb.save('Doc_Excel.xlsx')
+
+os.system('libreoffice Doc_Excel.xlsx')
