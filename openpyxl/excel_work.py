8a54c1ee933f0d35bb7c1ba7cd2be56086b163da
Felipe Gonzalez Q <fgonzalez@lya-electronic.com>
2021-04-30 13:41:28 -0500

    c

--- openpyxl/excel_work.py
+++ openpyxl/excel_work.py
@@ -2,25 +2,38 @@ import os
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
+##########################
+### CREAR OBJETO EXCEL ###
+##########################
 wb = Workbook() # creamos objeto de Excel
 wb.save('Doc_Excel.xlsx') 
 # se guarda un documento de excel en blanco con
 # una sola ws llamada "sheet"
 
+###########################
+### CARGAR OBJETO EXCEL ###
+###########################
 # Para cargar una ws de excel ya existente
 wb = load_workbook('Doc_Excel.xlsx')
+
+### CREAMOS UNA HOJA DE TRABAJO ###
 wb.create_sheet('Primera_ws', # nombre de la hoja
 0) # posición de la hoja
 
+
+###########################
+### DATOS DE LAS CELDAS ###
+###########################
+# Forma 1:
+ws['B1'] = 1
 # Escribiremos valores en la primero columna
 for i in range(10):
     wb['Primera_ws']['A{}'.format(i+1)] = i 
-# Otra forma de cambiar los valores
+# Forma 2:
 ws = wb['Primera_ws']
 ws.cell(row=6, column=1, value='hola')
 
-ws['B1'] = 1
-
+### GUARDADMOS LOS CAMBIOS EN EL ARCHIVO ###
 wb.save('Doc_Excel.xlsx')
 
 # Cambiarle el estilo a la pestaña de la ventana
